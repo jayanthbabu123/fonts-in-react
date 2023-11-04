aeb4984d36fed2dc04c298b8ddf23fed7918e4d5
Jayanth babu S <sjayanth.ip@gmail.com>
2023-11-04 16:07:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <p>
+        When creating a React app, you may want to include custom fonts to
+        enhance the overall look and feel of your application. There are
+        multiple ways to include fonts in your React app, including using Google
+        Fonts or custom font files. In this tutorial, we will walk you through
+        the steps for including fonts in your React app using both Google Fonts
+        and custom font files.
+      </p>
+      <p>
+        When creating a React app, you may want to include custom fonts to
+        enhance the overall look and feel of your application. There are
+        multiple ways to include fonts in your React app, including using Google
+        Fonts or custom font files. In this tutorial, we will walk you through
+        the steps for including fonts in your React app using both Google Fonts
+        and custom font files.
+      </p>
+    </div>
+  );
+}
